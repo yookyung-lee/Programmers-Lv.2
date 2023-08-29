82cb102c874532bb6335a74e31cf0728638e637a
재맵 <119744720+yookyung-lee@users.noreply.github.com>
2023-08-29 12:49:37 +0900

    Update 최소직사각형.cpp
    It gives the same result in this file- i++ and ++i

--- 최소직사각형.cpp
+++ 최소직사각형.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int solution(vector<vector<int>> sizes) {
     int left = 0;
     int right = 0;
-    for (int i = 0; i < sizes.size(); ++i) {
+    for (int i = 0; i < sizes.size(); i++) {
         left = max(left, max(sizes[i][0], sizes[i][1])); //the longest length between longer side of a rectangle
         right = max(right, min(sizes[i][0], sizes[i][1])); //the longest length between shorter side of a rectangle
     }
